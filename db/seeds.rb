a72f1408663445425d2d63594fea22cc826d3f50
tony555t <tonytullah764@gmail.com>
2023-02-27 10:29:49 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# # Add a console message so we can see output when the seed file runs
-# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
-# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
-# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
-# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
-
-
-
-
+# Add a console message so we can see output when the seed file runs
 puts "Seeding games..."
 
 # run a loop 50 times
